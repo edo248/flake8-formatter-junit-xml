b1df37d388b32b3761e30b5f5dc48a3a7ac95c42
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-12-20 10:12:24 +0900

    Write to stdout as default format

--- flake8_formatter_junit_xml/formatter.py
+++ flake8_formatter_junit_xml/formatter.py
@@ -1,17 +1,22 @@
-from flake8.formatting import base
+from __future__ import print_function
+from flake8.formatting import default
 from junit_xml import TestSuite, TestCase
 
 
-class JUnitXmlFormatter(base.BaseFormatter):
+class JUnitXmlFormatter(default.Default):
     """JUnit XML formatter for Flake8."""
 
     test_suites = dict()
+
+    def after_init(self):
+        self.options.format = "default" # so that DefaultFormatter uses their built-in format
+        super().after_init()
 
     def beginning(self, filename):
         name = '{0}.{1}'.format("flake8", filename.replace('.', '_'))
         self.test_suites[filename] = TestSuite(name, file=filename)
 
-    # Do not write each error
+    # Store each error as a TestCase
     def handle(self, error):
         name = '{0}, {1}'.format(error.code, error.text)
         test_case = TestCase(name, file=error.filename, line=error.line_number)
@@ -24,8 +29,19 @@ class JUnitXmlFormatter(base.BaseFormatter):
         }
         test_case.add_failure_info(message)
         self.test_suites[error.filename].test_cases.append(test_case)
+        super().handle(error)
+
+    # Only write to fd (unless None)
+    def _write_fd(self, output):
+        if self.output_fd is not None:
+            self.output_fd.write(output + self.newline)
+
+    # Only write to screen (if necessary)
+    def _write(self, output):
+        if self.output_fd is None or self.options.tee:
+            print(output)
 
     # writes results to file after all files are processed
     def stop(self):
-        self._write(TestSuite.to_xml_string(iter(self.test_suites.values())))
+        self._write_fd(TestSuite.to_xml_string(iter(self.test_suites.values())))
         super().stop()
